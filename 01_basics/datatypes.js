4eab9af228da3f17e66eec5ff249b4890e936df2
Digisk <152156875+Digisk@users.noreply.github.com>
2024-05-20 19:03:04 +0000

    working on datatypes and variables

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,15 @@
+let name = "sumit"
+let age = 26
+let isLoggedIn = false
+
+
+// number=> 2 to power 53
+// bigInt  use for big number of ammount
+// string = ""
+// boolean = true/false
+// null = standalone value
+// undefined => 
+
+// typeof section here----
+console.log(typeof null); // output is object 
+console.log(typeof undefined); // output is undefined 
---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+// learning about variables
+const accountId = 1234
+let accountEmail = "sumit@gmail.com"
+var accountPassword = "1234"
+accountCity = "Pune"
+
+// console.log(accountCity);
+// To print multiple variables..
+console.log([accountId, accountEmail,accountPassword,accountCity]);
+// To print multiple variables in table format..
+console.table([accountId, accountEmail,accountPassword,accountCity]);
