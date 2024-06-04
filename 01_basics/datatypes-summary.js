f890ce78b16d19b7db7f33e80fd39c3d8e9607c0
Digisk <152156875+Digisk@users.noreply.github.com>
2024-06-04 19:08:25 +0000

    operation and datatypes

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,21 @@
+// primitives dataTypes
+
+// Boolean String Number Null undefined Symbol BigInt
+
+// Non-primitives dataTypes (Refrence)
+
+// Array Object Functions
+
+// Array code
+const hero=["sumit", "sumit1", "sumit2", "sumit3"]
+
+// Object code
+let object= {
+    name : "sumit",
+    age : "26"
+}
+
+// function code
+const myFunction= function(){
+    console.log("Hello Sumit");
+}
---
+++ 01_basics/operation.js
@@ -0,0 +1,12 @@
+let value =3
+let NegValue =-value
+console.log(NegValue);
+
+
+// add string 
+let str1= "Hello" 
+
+
+let str2= " Sumit"
+let str3= str1+str2
+console.log(str3);
